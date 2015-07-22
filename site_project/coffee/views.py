7d31c635f67cea1f7310b612b951b14777722a16
Phil Middleton <pjmiddle@mtu.edu>
2015-07-21 22:52:32 -0400

    Just add some stuff to the views

--- site_project/coffee/views.py
+++ site_project/coffee/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import RequestContext, loader
+
+from .models import Coffee
 
 # Create your views here.
 def index(request):
--- site_project/movement/views.py
+++ site_project/movement/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Activity, Fitbit
+
 # Create your views here.
 def index(request):
 	return HttpResponse("Movement test")
